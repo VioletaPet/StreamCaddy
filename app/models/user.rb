5a5f49dc2e3eab3517e030fd86dd3889cefef0a0
Eliot Lila <147817846+EliotL1@users.noreply.github.com>
2024-12-10 14:22:29 +0100

    testing and fix modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_providers
   has_many :watch_providers, through: :user_providers
+  has_many :watchlist_media
+  has_many :medias, through: :watchlist_media
+  has_many :media_genres, through: :medias
+  has_many :genres, through: :media_genres
+  has_many :seasons, through: :medias
+  has_many :episodes, through: :seasons
   validates :first_name, :last_name, presence: true
   validates :email, :username, uniqueness: true
 end
--- app/models/watch_provider.rb
+++ app/models/watch_provider.rb
@@ -1,4 +1,6 @@
 class WatchProvider < ApplicationRecord
+  has_many :user_providers
+  has_many :media_watch_providers
   has_many :users, through: :user_providers
   has_many :medias, through: :media_watch_providers
 end
