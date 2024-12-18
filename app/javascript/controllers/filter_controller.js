4b5c74aae5bc6abaf0e9cdd4f3e9f5aba6f58083
Angy Hijaze <angy.hijaze@gmail.com>
2024-12-18 14:37:51 +0100

    incomplete with media index

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -3,27 +3,37 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["movies", "tvshows"];
 
+filterByGenre(event) {
+  const selectedGenre = event.target.value;
+
+  const mediaCards = document.querySelectorAll("[data-genre]");
+
+
+  mediaCards.forEach((card) => {
+    const cardGenre = card.dataset.genre;
+
+
+    if (selectedGenre === "all" || cardGenre === selectedGenre) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
 
   filterContent(event) {
-    const selectedType = event.target.value;
+    const selectedFilter = event.target.value;
 
-    if (selectedType === "all") {
-      this.showAll();
-    } else if (selectedType === "movies") {
-      this.filterByType("movies");
-    } else if (selectedType === "tvshows") {
-      this.filterByType("tvshows");
-    }
-  }
-
-
-  filterByGenre(event) {
-    const selectedGenre = event.target.value;
-
-    if (selectedGenre === "all") {
-      this.showAll();
-    } else {
-      this.filterBySelectedGenre(selectedGenre);
+    switch (selectedFilter) {
+      case "all":
+        this.showAll();
+        break;
+      case "movies":
+        this.showMovies();
+        break;
+      case "tvshows":
+        this.showTVShows();
+        break;
     }
   }
 
@@ -32,36 +42,13 @@ export default class extends Controller {
     this.tvshowsTarget.style.display = "block";
   }
 
-  filterByType(type) {
-    const movieElements = this.moviesTarget.querySelectorAll("[data-type]");
-    const tvShowElements = this.tvshowsTarget.querySelectorAll("[data-type]");
-
-    movieElements.forEach(movie => movie.style.display = "none");
-    tvShowElements.forEach(show => show.style.display = "none");
-
-    if (type === "movies") {
-      movieElements.forEach(movie => movie.style.display = "block");
-    } else if (type === "tvshows") {
-      tvShowElements.forEach(show => show.style.display = "block");
-    }
+  showMovies() {
+    this.moviesTarget.style.display = "block";
+    this.tvshowsTarget.style.display = "none";
   }
 
-  filterBySelectedGenre(genre) {
-    const movieElements = this.moviesTarget.querySelectorAll("[data-genre]");
-    const tvShowElements = this.tvshowsTarget.querySelectorAll("[data-genre]");
-
-    movieElements.forEach(movie => movie.style.display = "none");
-    tvShowElements.forEach(show => show.style.display = "none");
-
-    movieElements.forEach(movie => {
-      if (movie.getAttribute("data-genre") === genre) {
-        movie.style.display = "block";
-      }
-    });
-    tvShowElements.forEach(show => {
-      if (show.getAttribute("data-genre") === genre) {
-        show.style.display = "block";
-      }
-    });
+  showTVShows() {
+    this.moviesTarget.style.display = "none";
+    this.tvshowsTarget.style.display = "block";
   }
 }
